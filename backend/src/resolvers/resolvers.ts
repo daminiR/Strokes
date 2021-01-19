80d11756eb4ceb1bb54a7ab016813dc23bec692f
Damini Rijhwani <rijhwanidamini@gmail.com>
2021-01-19 02:10:09 -0500

    :bug: RN debugger bug set App init GLOBAL.Blob = null temp fix

--- backend/src/resolvers/resolvers.ts
+++ backend/src/resolvers/resolvers.ts
@@ -2,19 +2,7 @@ import Squash from '../models/Squash';
 import { SquashDocument } from '../types/Squash.d'
 export const resolvers = {
   Mutation: {
-    createSquash: (
-      root,
-      args: {
-        first_name: string,
-        age: number,
-        gender: string,
-        sports: string,
-        game_level: string,
-        country: string,
-        description: string,
-        image_set: string,
-      }
-    ): Promise<SquashDocument> => {
+    createSquash: async (root, args): Promise<SquashDocument> => {
       const squash = Squash.create(args);
       return squash;
     },
---
+++ mobile/src/graphql/mutations/profile.ts
@@ -0,0 +1,10 @@
+import { gql, useMutation } from '@apollo/client'
+
+const ADD_PROFILE = gql `
+  mutation CreateSquash($first_name: String!){
+    createSquash(first_name: $first_name){
+      first_name
+    }
+  }
+`
+export { ADD_PROFILE }
