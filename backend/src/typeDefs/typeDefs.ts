9d8b45539173f5b1542c395bdcc1a87d54a0970a
Damini Rijhwani <drijhwan@purdue.edu>
2021-01-21 21:04:40 -0500

    :bug: temp fix for debugger

--- backend/src/typeDefs/typeDefs.ts
+++ backend/src/typeDefs/typeDefs.ts
@@ -9,17 +9,17 @@ export const typeDefs = gql`
   input SquashNode {
     sport: String!
     isUserSport: Boolean
-    }
+  }
   type Mutation {
     createSquash(
       first_name: String!
-      age: Int
-      gender: String
-      sports: [SquashNode]
-      game_level:String
+      age: Int!
+      gender: String!
+      sports: [SquashNode!]!
+      game_level: String!
       country: String
       description: String
-      image_set: [String]
+      image_set: [String!]!
     ): Squash!
   }
 `;
