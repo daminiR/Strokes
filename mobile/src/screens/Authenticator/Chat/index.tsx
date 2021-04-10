32eefd63bf1a312f1e3ca56368e2391dbd098725
Damini Rijhwani <rijhwanidamini@gmail.com>
2021-04-10 15:03:23 -0400

    too many name changes, and nav changes -> lots of bugs to fix

---
+++ mobile/src/screens/Authenticator/Chat/index.tsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState, ReactElement } from 'react'
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack'
+import { View, Text, Button } from 'react-native'
+import { onScreen, goBack } from '../../../constants'
+import { AppContainer } from '../../../components'
+import { RootStackParamList } from '../../../AppNavigator'
+import auth from '@react-native-firebase/auth'
+import {UserContext} from '../../../UserContext'
+import { useQuery, useMutation } from '@apollo/client'
+import { GET_PROFILE_STATUS, READ_SQUASH, GET_SELECTED_SQUASH, READ_SQUASHES } from '../../../graphql/queries/profile'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+type MatchScreenNavigationProp = StackNavigationProp<RootStackParamList, 'MATCH'>
+
+type MatchT = {
+  navigation: MatchScreenNavigationProp
+}
+
+const Match  = ({ navigation }: MatchT ): ReactElement => {
+  //<Button title="Profile" onPress={_onPressProfile}/>
+
+  return (
+    <>
+      <AppContainer title="User" loading={loading}>
+        <Text>
+          {'this is matching page'}
+        </Text>
+      </AppContainer>
+    </>
+  );
+}
+export { Match }
