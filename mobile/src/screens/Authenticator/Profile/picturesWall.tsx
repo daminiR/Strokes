77b71babe4020dbaa90bc92f9d21780cb5135a6d
daminiR <rijhwanidamini@gmail.com>
2021-08-29 23:54:10 -0400

    yass! front view for profile is done, time to integrate backend

--- mobile/src/screens/Authenticator/Profile/picturesWall.tsx
+++ mobile/src/screens/Authenticator/Profile/picturesWall.tsx
@@ -1,61 +1,95 @@
-import {Button, Avatar, Badge } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import {Button,withBadge, Icon, Avatar, Badge } from 'react-native-elements'
 import React from 'react'
-import {View, StyleSheet } from 'react-native'
+import { ProfileSettingsInput } from "./profileSettingInput"
+import {View, ScrollView, StyleSheet } from 'react-native'
+
+
 
 const PictureWall = (props) => {
-    //<Button icon={cancelProps} type="clear"}/>
+  return (
+    <>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.top}>
+          <View style={styles.verticalImageplaceholder}>
+            <View style={styles.horizontalImageplaceholder}>
+              <SingleImagePlaceholder />
+              <SingleImagePlaceholder />
+              <SingleImagePlaceholder />
+            </View>
+            <View style={styles.horizontalImageplaceholder}>
+              <SingleImagePlaceholder />
+              <SingleImagePlaceholder />
+              <SingleImagePlaceholder />
+            </View>
+          </View>
+        </View>
+        <View style={styles.middle}>
+          <ProfileSettingsInput/>
+        </View>
+      </View>
+    </ScrollView>
+    </>
+  );
+}
+
+const SingleImagePlaceholder = (props) => {
     const cancelProps = {
         name:"close-circle-outline",
             type:"material-community",
             size: 30}
     return (
-        <>
-            <View style={styles.container}>
-                <View style={styles.top}>
-<Avatar
-  size={120}
-  icon={{name: 'camera-plus-outline', type: 'material-community'}}
-  overlayContainerStyle={{backgroundColor: '#D3D3D3'}}
-  onPress={() => console.log("Works!")}
-  activeOpacity={0.7}
-    containerStyle={{marginLeft: 0, marginTop: 0, top: 20, left: 20}}>
-</Avatar>
-
-                 </View>
-                <View style={styles.middle} />
-        <View style={styles.bottom} />
-                 </View>
-        </>
-    )
+      <>
+            <Avatar
+              size={130}
+              icon={{name: 'camera-plus-outline', type: 'material-community'}}
+              overlayContainerStyle={{
+                backgroundColor: '#D3D3D3',
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+                borderBottomLeftRadius: 20,
+                borderBottomRightRadius: 20,
+              }}
+              onPress={() => console.log('Works!')}
+              activeOpacity={0.7}
+              containerStyle={{
+                padding: 0,
+                marginLeft: 0,
+                marginTop: 0,
+              }}>
+              <Avatar.Accessory {...cancelProps} />
+            </Avatar>
+      </>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
-    backgroundColor: "#fff",
-    padding: 10,
-    margin: 10,
+    backgroundColor: '#fff',
+    padding: 2,
+    margin: 0,
   },
   top: {
-    flex: 0.3,
-    backgroundColor: "white",
+    flex: 0.9,
+    margin: 1,
+    backgroundColor: 'white',
     borderWidth: 5,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius:20,
   },
   middle: {
-    flex: 0.3,
-    backgroundColor: "beige",
+    flex: 1.1,
+    backgroundColor: 'white',
     borderWidth: 5,
   },
-  bottom: {
-    flex: 0.3,
-    backgroundColor: "pink",
-    borderWidth: 5,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+  horizontalImageplaceholder: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  verticalImageplaceholder: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 });
 
