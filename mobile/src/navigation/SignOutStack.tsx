cc7935af6f6f20bec1b9e7480be234664f0b70bc
Damini Rijhwani <rijhwanidamini@gmail.com>
2021-10-06 00:27:57 -0400

    editing on mac :apple:

--- mobile/src/navigation/SignOutStack.tsx
+++ mobile/src/navigation/SignOutStack.tsx
@@ -2,13 +2,14 @@ import React, { useContext, useState, ReactElement } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Email, Hello, SignIn, ConfirmSignUp} from '../screens/Authenticator'
 import { UserContext } from '../UserContext'
-import {ProfileImages, ProfileComplete, ProfileSport, ProfileGender, ProfileName, ProfileBirthday} from '../screens/Authenticator'
+import {ProfileImages, ProfileComplete, ProfileSport,SignUp,  ProfileGender, ProfileName, ProfileBirthday} from '../screens/Authenticator'
 import { NavigationContainer } from '@react-navigation/native'
 
 const Stack = createStackNavigator()
 
 export type RootStackSignOutParamList = {
   HELLO: undefined
+  SIGNUP: undefined
   SIGN_IN: undefined
   CONFIRM_SIGN_UP: undefined
   EMAIL : undefined
@@ -24,6 +25,7 @@ export default function SignOutStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
+      <Stack.Screen name="SIGNUP" component={SignUp} />
       <Stack.Screen name="HELLO" component={Hello} />
       <Stack.Screen name="SIGN_IN" component={SignIn} />
       <Stack.Screen name="CONFIRM_SIGN_UP" component={ConfirmSignUp} />
