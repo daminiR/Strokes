9bca6db37c55595492d367faa43461c54e6b54a6
producthaul <trialproducthaul@gmail.com>
2021-11-18 18:01:30 -0500

    google places is working now

--- mobile/src/components/NeighborhoodSearch/index.tsx
+++ mobile/src/components/NeighborhoodSearch/index.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useContext, useState, ReactElement } from 'react'
+import React, { useRef, useEffect, useContext, useState, ReactElement } from 'react'
 import { useFormikContext} from 'formik';
 import { StackNavigationProp } from '@react-navigation/stack'
-import {ScrollView, StyleSheet } from 'react-native'
 import { genderRadioObject } from '../../constants'
 import auth from '@react-native-firebase/auth'
 import {Theme, Text, Chip, Card, Input, Button,withBadge, ListItem, Icon, Avatar, Badge } from 'react-native-elements'
@@ -13,19 +12,28 @@ import {Pictures} from '..'
 import {ChooseSportsChips} from '..'
 import { EditFields, ProfileFields, SignIn} from '../../localModels/UserSportsList'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {API_KEY} from './API_KEY'
+import {ScrollView } from 'react-native'
 
 const GooglePlacesInput = () => {
+    const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.setAddressText('Some Text2');
+  }, []);
   return (
       <ScrollView>
     <GooglePlacesAutocomplete
+        ref={ref}
       placeholder='Search'
       onPress={(data, details = null) => {
         // 'details' is provided when fetchDetails = true
         console.log(data, details);
       }}
       query={{
-        key: 'AIzaSyCxdvZ1p2d-AP7n20cKHzR03tCxPmk1Dyw',
+        key: API_KEY,
         language: 'en',
+        components: 'country:us',
       }}
     />
       </ScrollView>
