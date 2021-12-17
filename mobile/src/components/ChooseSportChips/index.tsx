882982d1c81c43285df5001510dae2948b7cf176
Damini Rijhwani <rijhwanidamini@gmail.com>
2021-12-17 16:22:02 -0500

    ediitng on linux

--- mobile/src/components/ChooseSportChips/index.tsx
+++ mobile/src/components/ChooseSportChips/index.tsx
@@ -1,16 +1,67 @@
 import {Card} from 'react-native-elements'
-import React from 'react'
+import React, {useState} from 'react'
 import {View} from 'react-native';
 import {sportsList} from '../../constants';
 import styles from '../../assets/styles'
 import {SportChips} from '../SportsChips'
+import { Cancel, Done, } from '..'
 import { EditFields} from '../../localModels/UserSportsList'
 import { useFormikContext} from 'formik';
+import {Overlay, CheckBox, Text} from 'react-native-elements'
+
+
+const GameLevelChoose = ({isVisible}) => {
+  const [gameLevelVisible, setGameLevelVisible] = useState(false)
+  const [gameLevel1, setGameLevel1] = useState(false);
+  const [gameLevel2, setGameLevel2] = useState(false);
+  const [gameLevel0, setGameLevel0] = useState(false);
+  const _onPressGameLevel = (gameLevel) => {
+    switch (gameLevel) {
+      case 0:
+        setGameLevel0(!gameLevel0);
+        break;
+      case 1:
+        setGameLevel1(!gameLevel1);
+        break;
+      case 2:
+        setGameLevel2(!gameLevel2);
+        break;
+    }
+  };
+  return(
+    <Overlay isVisible={isVisible}>
+          <View style={{marginTop: 20}}>
+            <Text style={styles.filtersText}>Filter level for:</Text>
+            <CheckBox
+              title="Beginner"
+              checked={gameLevel0}
+              onPress={() => _onPressGameLevel(0)}
+            />
+            <CheckBox
+              title="Intermediate"
+              checked={gameLevel1}
+              onPress={() => _onPressGameLevel(1)}
+            />
+            <CheckBox
+              title="Advanced"
+              checked={gameLevel2}
+              onPress={() => _onPressGameLevel(2)}
+            />
+          </View>
+    </Overlay>
+  )
+}
+
+
+
+
 
 const ChooseSportsChips = () => {
   const {setFieldValue, values: formikValues} = useFormikContext<EditFields>();
+  const [gameLevelVisible, setGameLevelVisible] = useState(false)
   const getData = (newSport, isSelected) => {
     if(isSelected){
+      setGameLevelVisible(true)
       const sportObj = [{sport: newSport, game_level: 0}];
       if (formikValues.sports != null){
       const new_values = formikValues.sports.concat(sportObj)
@@ -19,7 +70,6 @@ const ChooseSportsChips = () => {
       else{
       setFieldValue('sports', sportObj)
       }
-
     }
     else{
       const allSports = formikValues.sports
@@ -31,6 +81,7 @@ const ChooseSportsChips = () => {
   const renderFormikSports = () => {
     return (
       <>
+        <GameLevelChoose isVisible={gameLevelVisible}/>
         <Card containerStyle={styles.CardStyle}>
           <Card.Title> List of Acitivities</Card.Title>
           <Card.Divider />
