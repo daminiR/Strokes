291d4e77d9861dd9905a83ac3eb221b41dbbb4dc
Damini Rijhwani <rijhwanidamini@gmail.com>
2022-05-22 23:56:47 -0400

    changed resolver to hande individual auth, 2/3 querries without auth needed

--- backend/src/resolvers/updateUser.ts
+++ backend/src/resolvers/updateUser.ts
@@ -8,28 +8,30 @@ import {
   SPORT_CHANGES_PER_DAY,
 } from "../constants/";
 import sanitize from 'mongo-sanitize'
+import { AuthenticationError }  from 'apollo-server-express';
 
 
 export const resolvers = {
   Query: {
     squash: async (parents, unSanitizedId, context, info) => {
-      const {id} = sanitize(unSanitizedId)
-      console.log(id)
-      const squash_val = await Squash.findById(id)
+      const user = context.user;
+      const { id } = sanitize(unSanitizedId);
+      if (user?.uid != id) throw new AuthenticationError("not logged in");
+      console.log(id);
+      const squash_val = await Squash.findById(id);
       console.log(squash_val);
       return squash_val;
     },
     squashes: async (parents, unSanitizedId, context, info) => {
-      const {id} =  sanitize(unSanitizedId)
-      const squashes = await Squash.find({id});
+      const user = context.user;
+      const { id } = sanitize(unSanitizedId);
+      if (user?.uid != id) throw new AuthenticationError("not logged in");
+      const squashes = await Squash.find({ id });
       return squashes;
     },
   },
   Mutation: {
-    updateUserProfile: async (
-      root,
-      unSanitizedData,
-    ) => {
+    updateUserProfile: async (root, unSanitizedData) => {
       const {
         _id,
         image_set,
@@ -43,18 +45,27 @@ export const resolvers = {
         remove_uploaded_images,
         add_local_images,
         original_uploaded_image_set,
-      } = sanitize(unSanitizedData)
+      } = sanitize(unSanitizedData);
+
       //const removed_image_set = await deleteFilesFromGC(remove_uploaded_images, original_uploaded_image_set, add_local_images.length);
-      const removed_image_set = await deleteFilesFromAWS(remove_uploaded_images, original_uploaded_image_set, add_local_images.length);
+      const removed_image_set = await deleteFilesFromAWS(
+        remove_uploaded_images,
+        original_uploaded_image_set,
+        add_local_images.length
+      );
       //const data_set = await creatGCUpload(add_local_images, _id)
-      const data_set = await createAWSUpload(add_local_images, _id)
-      const final_image_set = removed_image_set.concat(data_set)
+      const data_set = await createAWSUpload(add_local_images, _id);
+      const final_image_set = removed_image_set.concat(data_set);
       // if sport changes decrement sportsPerDay
-      const check_doc_sports = await Squash.findById( _id);
-      var sportChangesPerDay = check_doc_sports?.sportChangesPerDay
+      const check_doc_sports = await Squash.findById(_id);
+      var sportChangesPerDay = check_doc_sports?.sportChangesPerDay;
       // only check if list of sports are the same, user shoul dbe allowed to change gamelevels freely
-      const sportsOld = _.map(check_doc_sports?.sports, (sportObj) => {return sportObj.sport})
-      const sportsNew = _.map(sports, (sportObj) => {return sportObj.sport})
+      const sportsOld = _.map(check_doc_sports?.sports, (sportObj) => {
+        return sportObj.sport;
+      });
+      const sportsNew = _.map(sports, (sportObj) => {
+        return sportObj.sport;
+      });
       //if (!_.isEqual(sportsOld, sportsNew)) {
       if (!_.isEqual(check_doc_sports?.sports, sports)) {
         sportChangesPerDay = sportChangesPerDay
@@ -62,24 +73,24 @@ export const resolvers = {
           : SPORT_CHANGES_PER_DAY;
       }
       const doc = await Squash.findOneAndUpdate(
-          { _id: _id },
-          {
-            $set: {
-              _id: _id,
-              image_set: final_image_set,
-              first_name: first_name,
-              last_name: last_name,
-              gender: gender,
-              location: location,
-              age: age,
-              sports: sports,
-              description: description,
-              sportChangesPerDay: sportChangesPerDay,
-            },
+        { _id: _id },
+        {
+          $set: {
+            _id: _id,
+            image_set: final_image_set,
+            first_name: first_name,
+            last_name: last_name,
+            gender: gender,
+            location: location,
+            age: age,
+            sports: sports,
+            description: description,
+            sportChangesPerDay: sportChangesPerDay,
           },
-          { new: true }
-        );
-    return doc
+        },
+        { new: true }
+      );
+      return doc;
     },
-  }
-}
+  },
+};
