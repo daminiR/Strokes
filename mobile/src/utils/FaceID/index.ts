3c724b068b5a27b275ee7034e5b6fc97d31fc182
Damini Rijhwani <rijhwanidamini@gmail.com>
2022-10-10 17:20:00 -0400

    simple fixes added

--- mobile/src/utils/FaceID/index.ts
+++ mobile/src/utils/FaceID/index.ts
@@ -1,18 +1,23 @@
 import * as Keychain from 'react-native-keychain';
 
 const setNewKeychain = (phoneNumber, password) =>{
+    var settings =  { service: 'org.reactjs.native.example.sports-app-keychain-password' };
     Keychain.setGenericPassword(phoneNumber, password, {
+      service: 'org.reactjs.native.example.sports-app-keychain-password',
       accessControl: Keychain.ACCESS_CONTROL.BIOMETRY_CURRENT_SET_OR_DEVICE_PASSCODE,
                           accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
       authenticationType: Keychain.AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
-    }).then(() => {
+    }).then((data) => {
+      console.log('data',data)
                         }).catch((error) => {
                           console.log(error)
                         })
 }
-const getPassword = (setCredentials, setFieldValue, setIsFaceID) =>{
+const getPassword = async (setCredentials, setFieldValue, setIsFaceID) =>{
+  return await new Promise((resolve, reject) => {
     try {
-        Keychain.getGenericPassword().then((credentials) => {
+      const settings = {service: 'org.reactjs.native.example.sports-app-keychain-password'}
+        Keychain.getGenericPassword(settings).then((credentials) => {
         console.log(credentials)
            if (credentials) {
             setCredentials(credentials)
@@ -20,11 +25,21 @@ const getPassword = (setCredentials, setFieldValue, setIsFaceID) =>{
             setFieldValue( 'password', credentials.password)
             setIsFaceID(true)
            }
-         }).catch(() => {
+         resolve()
+
+         }
+                                                  )
+                                                  .catch(() => {
+                                                    reject()
          })
 
   } catch (error) {
+    reject()
     console.log("Keychain couldn't be accessed!", error);
   }
+  })
+
+
+
 }
 export {setNewKeychain, getPassword}
